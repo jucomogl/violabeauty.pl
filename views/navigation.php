1b4d5ba2576dc9a3f0c28fa3db89b006ab5d8d72
Greg <jucom@jucom.pl>
2024-10-23 09:40:55 +0100

    UPDATE NAV BUG

--- views/navigation.php
+++ views/navigation.php
@@ -1,3 +1,58 @@
+<aside class="side-widget">
+    <div class="inner">
+    <!-- Logo Menu Mobile -->
+      <div class="logo"> <a href="/"><img src="assets/logo.png" alt="Image"></a> </div>
+      <div class="hide-mobile">
+        <div class="or">
+            <h2 class="h2-baslik-hizmetler-2"> Informacje </h2>
+        </div>
+        <div class="bosluk2dt"></div>
+        <div class="iconsv"><i class="flaticon-call"></i></div>
+        <address class="address">
+        <p><a href="#">+48 538 903 484</a>
+            <div class="bosluk2dt"></div>
+            <div class="iconsv"><i class="flaticon-email"></i></div>
+            <a href="#">booking@violabeauty.pl</a>
+            <div class="bosluk2dt"></div>
+            <div class="iconsv"><i class="flaticon-location"></i></div>
+            <a href="#">Wrocław PL</a>
+            <div class="bosluk2dt"></div>
+            <div class="or">
+                <a href="#"><img src="img/facebook1.png" alt=""></a>
+                <a href="#"><img src="img/instagram1.png" alt=""></a>
+                <a href="#"><img src="img/twitter1.png" alt=""></a>
+                <a href="#"><img src="img/google1.png" alt=""></a>
+            </div>
+        </p>
+        </address>
+      </div>
+      <div class="show-mobile">
+        <div class="site-menu">
+            <ul class="menu">
+                <li><a href="?page=home">Start</a></li>
+                <li><a href="?page=about">O nas</a></li>
+                <li><a href="?page=services">Usługi </a><span class="sb" style="font-size: 18px">+</span>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="lip-design.html">Lip Design</a></li>
+                        <li><a class="dropdown-item" href="skin-care.html">Skin Care</a></li>
+                        <li><a class="dropdown-item" href="permanent-makeup.html">Permanent Makeup</a></li>
+                        <li><a class="dropdown-item" href="regional-slimming.html">Regional Slimming</a></li>
+                        <li><a class="dropdown-item" href="epilation.html">Epilation</a></li>
+                        <li><a class="dropdown-item" href="eyebrow-design.html">Eyebrow Design</a></li>
+                      </ul>
+                </li>
+                <li><a href="?page=before-after">Przed i Po</a></li>
+                <!--<li><a href="our-price-list.html">Our Price List</a></li>-->
+                <li><a href="?page=gallery">Realizacje</a></li>
+                <!--<li><a href="blog.html">Blog</a></li>-->
+                <li><a href="?page=contact-us">Kontakt</a></li>
+            </ul>
+          </div>
+      </div>
+      <small>� 2024- Voleta Chumakova</small> </div>
+  </aside>
+ 
+ 
  <nav class="navbar">
             <div class="container">
             <!-- Logo Menu Desktop -->
